c6f38816fc281ac5a7d1390b634ffcbcec8e9506
Volodimir <vova19941@ukr.net>
2022-09-27 09:27:00 +0300

    add Pages in menu

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -7,6 +7,9 @@ import ProductListSix from 'components/Products/ProductListSix'
 import { Container } from '@mui/system'
 import { Routes, Route } from 'react-router-dom'
 import CartPage from 'pages/CartPage/CartPage'
+import AboutPage from 'pages/AboutPage/AboutPage'
+import NewsPage from 'pages/NewsPage/NewsPage'
+import ContactPage from 'pages/ContactPage/ContactPage'
 
 type Props = {}
 
@@ -29,6 +32,9 @@ const Main = (props: Props) => {
                         }
                     />
                     <Route path="cart" element={<CartPage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="news" element={<NewsPage />} />
+                    <Route path="contact" element={<ContactPage />} />
                 </Routes>
             </Container>
         </>
---
+++ src/pages/NewsPage/NewsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const NewsPage = (props: Props) => {
+    return <div>NewsPage</div>
+}
+
+export default NewsPage
